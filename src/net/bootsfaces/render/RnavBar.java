7d22393a4f60d7957a4ef6f31638e60ba4c44f4d
droidcoder <droidcoder@thecoder4eu>
2015-02-16 09:07:02 +0100

    Small fix to NavBar renderer .

--- src/net/bootsfaces/render/RnavBar.java
+++ src/net/bootsfaces/render/RnavBar.java
@@ -38,9 +38,9 @@ public enum RnavBar {
     public static final String NAVBARBRAND=NAVBAR+"-brand";
     public static final String NAVBARINNER=NAVBAR+"-inner";
     public static final String NAVBARINVERSE=NAVBAR+" navbar-inverse";
-    public static final String NAVBARFIXBOTTOM=NAVBAR+" navbar-fixed-bottom";
-    public static final String NAVBARFIXTOP=NAVBAR+" navbar-fixed-top";
-    public static final String NAVBARSTATICTOP=NAVBAR+" navbar-static-top";
+    public static final String NAVBARFIXBOTTOM=" navbar-fixed-bottom";
+    public static final String NAVBARFIXTOP=" navbar-fixed-top";
+    public static final String NAVBARSTATICTOP=" navbar-static-top";
     
     /**
      * Begin Encoding
@@ -89,10 +89,10 @@ public enum RnavBar {
         
         if (inverse) { ns = NAVBARINVERSE; } else { ns = NAVBARDEFAULT; }
         if (fixed != null) {
-            if (fixed.equals(A.TOP)) { ns = NAVBARFIXTOP; }
-            if (fixed.equals(A.BOTTOM)) { ns = NAVBARFIXBOTTOM; }
+            if (fixed.equals(A.TOP)) { ns += NAVBARFIXTOP; }
+            if (fixed.equals(A.BOTTOM)) { ns += NAVBARFIXBOTTOM; }
         }
-        if (sttc) { ns = NAVBARSTATICTOP; }
+        if (sttc) { ns += NAVBARSTATICTOP; }
 
         /* 
          * The <nav> tag defines a set of navigation links. The <nav> element is intended only for major block of navigation links.
