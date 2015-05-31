9103df88c27f102b4a74bfcf28871898e5ccb085
yersan <legobrunch@gmail.com>
2015-05-31 11:33:57 +0100

    arquillian-infrastructure: Additional test configuration for b:column tag

--- src/test/java/net/bootsfaces/layout/ColumnITCase.java
+++ src/test/java/net/bootsfaces/layout/ColumnITCase.java
@@ -1,10 +1,11 @@
 /*
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
-*/
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package net.bootsfaces.layout;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import org.jboss.arquillian.container.test.api.Deployment;
@@ -16,8 +17,15 @@ import org.jboss.shrinkwrap.api.GenericArchive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.importer.ExplodedImporter;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.After;
+import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
@@ -25,38 +33,56 @@ import org.openqa.selenium.WebDriver;
  *
  * @author yersan
  */
-
 @RunWith(Arquillian.class)
 public class ColumnITCase {
-    
+
     @ArquillianResource
     private URL context;
-    
+
     public final static String WEBAPP_SRC = "target/test-classes/webapp/";
-    
+
     @Drone
     private WebDriver browser;
-    
-    
+
     @Deployment(testable = false)
     public static WebArchive createDeployment() {
         WebArchive deployment = ShrinkWrap.create(WebArchive.class, "test.war");
-        deployment.merge(ShrinkWrap.create(GenericArchive.class).as(ExplodedImporter.class)
-                .importDirectory(WEBAPP_SRC).as(GenericArchive.class),
-                "/", Filters.includeAll());
-        
+
+        deployment.merge(ShrinkWrap.create(GenericArchive.class)
+                .as(ExplodedImporter.class)
+                .importDirectory(WEBAPP_SRC)
+                .as(GenericArchive.class), "/", Filters.includeAll())
+                .addAsLibraries(new File("target/bootsfaces.jar"));
+
         System.out.println(deployment.toString(true));
-        
+
         return deployment;
     }
+
+    @Rule
+    public TestRule watcher = new TestWatcher() {
+        @Override
+        protected void starting(Description description) {
+            System.out.println("Starting test: " + description.getMethodName());
+        }
+    };
+
+    
     
     @Before
     public void setup() throws IOException {
         browser.get(context + "/faces/columnIT.xhtml");
     }
-    
+
     @Test
-    public void should_login_successfully() {
-        
+    public void renderColumn() {
+        String pageTitle = browser.getTitle();
+        Assert.assertEquals("Column IT", pageTitle);
+    }
+    
+    @After
+    public void after() {
+        System.out.println(browser.getPageSource());
+        browser.manage().deleteAllCookies();
     }
 }
