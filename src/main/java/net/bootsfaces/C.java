6f91dd571f1b4902af2f9e6230a4ac2da0fce452
stephanrauh <stephan@beyondjava.de>
2015-06-04 21:32:31 +0200

    implemented n:navCommandLink (Issue #75)

--- src/main/java/net/bootsfaces/C.java
+++ src/main/java/net/bootsfaces/C.java
@@ -68,6 +68,7 @@ public final class C {
     public static final String NAVBAR_COMPONENT_TYPE=BSFCOMPONENT+".NavBar";
     public static final String NAVBARLINKS_COMPONENT_TYPE=BSFCOMPONENT+".NavBarLinks";
     public static final String NAVLINK_COMPONENT_TYPE=BSFCOMPONENT+".NavLink";
+    public static final String NAVCOMMANDLINK_COMPONENT_TYPE=BSFCOMPONENT+".NavCommandLink";
     public static final String PANELGRID_COMPONENT_TYPE=BSFCOMPONENT+".PanelGrid";
     public static final String POLL_COMPONENT_TYPE=BSFCOMPONENT+".Poll";
     public static final String REMOTE_COMMAND_COMPONENT_TYPE=BSFCOMPONENT+".RemoteCommand";
