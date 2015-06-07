b1127da948608db88856034c6162658ca8636871
stephanrauh <stephan@beyondjava.de>
2015-06-07 11:07:52 +0200

    Started to refactor the panel component (but still work in progress)

---
+++ src/main/java/net/bootsfaces/component/panel/Panel.java
@@ -0,0 +1,396 @@
+/**
+ *  Copyright 2014-15 by Riccardo Massera (TheCoder4.Eu) and Stephan Rauh (http://www.beyondjava.net).
+ *  
+ *  This file is part of BootsFaces.
+ *  
+ *  BootsFaces is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  BootsFaces is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with BootsFaces. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.bootsfaces.component.panel;
+
+import javax.faces.application.ResourceDependencies;
+import javax.faces.application.ResourceDependency;
+import javax.faces.component.*;
+import net.bootsfaces.render.Tooltip;
+
+
+/** This class holds the attributes of &lt;b:panel /&gt;. */
+@ResourceDependencies({
+	@ResourceDependency(library="bsf", name="css/core.css"),
+        @ResourceDependency(library="bsf", name="css/panels.css"),
+        @ResourceDependency(library="bsf", name="css/bsf.css"),
+        @ResourceDependency(library="bsf", name="js/collapse.js", target="body")
+})
+@FacesComponent("net.bootsfaces.component.panel.Panel")
+public class Panel extends UIInput  implements net.bootsfaces.render.IHasTooltip  {
+	
+	public static final String COMPONENT_TYPE = "net.bootsfaces.component.panel.Panel";
+	
+	public static final String COMPONENT_FAMILY = "net.bootsfaces.component";
+	
+	public static final String DEFAULT_RENDERER = "net.bootsfaces.component.panel.Panel";
+	
+	public Panel() {
+		
+		
+	Tooltip.addResourceFile();
+		setRendererType(DEFAULT_RENDERER);
+	}
+	
+	public String getFamily() {
+		return COMPONENT_FAMILY;
+	}
+	
+	
+    protected enum PropertyKeys {
+binding,
+collapsed,
+collapsible,
+contentClass,
+contentStyle,
+id,
+look,
+style,
+styleClass,
+title,
+titleClass,
+titleStyle,
+tooltip,
+tooltipDelay,
+tooltipDelayHide,
+tooltipDelayShow,
+tooltipPosition
+;
+
+        String toString;
+
+        PropertyKeys(String toString) {
+            this.toString = toString;
+        }
+
+        PropertyKeys() {}
+
+        public String toString() {
+            return ((this.toString != null) ? this.toString : super.toString());
+        }
+    }
+	
+
+	/**
+	 * An el expression referring to a server side UIComponent instance in a backing bean. <br />
+	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 */
+	public javax.faces.component.UIComponent getBinding() {
+		javax.faces.component.UIComponent value = (javax.faces.component.UIComponent)getStateHelper().eval(PropertyKeys.binding);
+		return  value;
+	}
+	
+	/**
+	 * An el expression referring to a server side UIComponent instance in a backing bean. <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setBinding(javax.faces.component.UIComponent _binding) {
+	    getStateHelper().put(PropertyKeys.binding, _binding);
+    }
+	
+
+	/**
+	 * Is the panel collapsed? <br />
+	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 */
+	public boolean isCollapsed() {
+		Boolean value = (Boolean)getStateHelper().eval(PropertyKeys.collapsed, false);
+		return (boolean) value;
+	}
+	
+	/**
+	 * Is the panel collapsed? <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setCollapsed(boolean _collapsed) {
+	    getStateHelper().put(PropertyKeys.collapsed, _collapsed);
+    }
+	
+
+	/**
+	 * By default, panels can be folded by clicking the title bar. Seting collapsible to false will suppress the collapse/expand feature. <br />
+	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 */
+	public boolean isCollapsible() {
+		Boolean value = (Boolean)getStateHelper().eval(PropertyKeys.collapsible, true);
+		return (boolean) value;
+	}
+	
+	/**
+	 * By default, panels can be folded by clicking the title bar. Seting collapsible to false will suppress the collapse/expand feature. <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setCollapsible(boolean _collapsible) {
+	    getStateHelper().put(PropertyKeys.collapsible, _collapsible);
+    }
+	
+
+	/**
+	 * contentClass is optional: if specified, the content will be displayed with this specific class <br />
+	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 */
+	public String getContentClass() {
+		String value = (String)getStateHelper().eval(PropertyKeys.contentClass);
+		return  value;
+	}
+	
+	/**
+	 * contentClass is optional: if specified, the content will be displayed with this specific class <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setContentClass(String _contentClass) {
+	    getStateHelper().put(PropertyKeys.contentClass, _contentClass);
+    }
+	
+
+	/**
+	 * Inline style of the content area. <br />
+	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 */
+	public String getContentStyle() {
+		String value = (String)getStateHelper().eval(PropertyKeys.contentStyle);
+		return  value;
+	}
+	
+	/**
+	 * Inline style of the content area. <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setContentStyle(String _contentStyle) {
+	    getStateHelper().put(PropertyKeys.contentStyle, _contentStyle);
+    }
+	
+
+	/**
+	 * Unique identifier of the component in a namingContainer. <br />
+	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 */
+	public String getId() {
+		String value = (String)getStateHelper().eval(PropertyKeys.id);
+		return  value;
+	}
+	
+	/**
+	 * Unique identifier of the component in a namingContainer. <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setId(String _id) {
+	    getStateHelper().put(PropertyKeys.id, _id);
+    }
+	
+
+	/**
+	 * Look of the Panel, can be primary, success, info, warning, danger. Default is warning. <br />
+	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 */
+	public String getLook() {
+		String value = (String)getStateHelper().eval(PropertyKeys.look);
+		return  value;
+	}
+	
+	/**
+	 * Look of the Panel, can be primary, success, info, warning, danger. Default is warning. <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setLook(String _look) {
+	    getStateHelper().put(PropertyKeys.look, _look);
+    }
+	
+
+	/**
+	 * Inline style of the entire panel area. <br />
+	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 */
+	public String getStyle() {
+		String value = (String)getStateHelper().eval(PropertyKeys.style);
+		return  value;
+	}
+	
+	/**
+	 * Inline style of the entire panel area. <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setStyle(String _style) {
+	    getStateHelper().put(PropertyKeys.style, _style);
+    }
+	
+
+	/**
+	 * Style class of the input element. <br />
+	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 */
+	public String getStyleClass() {
+		String value = (String)getStateHelper().eval(PropertyKeys.styleClass);
+		return  value;
+	}
+	
+	/**
+	 * Style class of the input element. <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setStyleClass(String _styleClass) {
+	    getStateHelper().put(PropertyKeys.styleClass, _styleClass);
+    }
+	
+
+	/**
+	 * Title is optional: if specified, the heading facet will not be rendered and the panel will render with a heading with this Title. <br />
+	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 */
+	public String getTitle() {
+		String value = (String)getStateHelper().eval(PropertyKeys.title);
+		return  value;
+	}
+	
+	/**
+	 * Title is optional: if specified, the heading facet will not be rendered and the panel will render with a heading with this Title. <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setTitle(String _title) {
+	    getStateHelper().put(PropertyKeys.title, _title);
+    }
+	
+
+	/**
+	 * TitleClass is optional: if specified, the title will be displayed with this specific class <br />
+	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 */
+	public String getTitleClass() {
+		String value = (String)getStateHelper().eval(PropertyKeys.titleClass);
+		return  value;
+	}
+	
+	/**
+	 * TitleClass is optional: if specified, the title will be displayed with this specific class <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setTitleClass(String _titleClass) {
+	    getStateHelper().put(PropertyKeys.titleClass, _titleClass);
+    }
+	
+
+	/**
+	 * Inline style of the title area. <br />
+	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 */
+	public String getTitleStyle() {
+		String value = (String)getStateHelper().eval(PropertyKeys.titleStyle);
+		return  value;
+	}
+	
+	/**
+	 * Inline style of the title area. <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setTitleStyle(String _titleStyle) {
+	    getStateHelper().put(PropertyKeys.titleStyle, _titleStyle);
+    }
+	
+
+	/**
+	 * The text of the tooltip. <br />
+	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 */
+	public String getTooltip() {
+		String value = (String)getStateHelper().eval(PropertyKeys.tooltip);
+		return  value;
+	}
+	
+	/**
+	 * The text of the tooltip. <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setTooltip(String _tooltip) {
+	    getStateHelper().put(PropertyKeys.tooltip, _tooltip);
+    }
+	
+
+	/**
+	 * The tooltip is shown and hidden with a delay. This value is the delay in milliseconds. Defaults to 0 (no delay). <br />
+	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 */
+	public String getTooltipDelay() {
+		String value = (String)getStateHelper().eval(PropertyKeys.tooltipDelay);
+		return  value;
+	}
+	
+	/**
+	 * The tooltip is shown and hidden with a delay. This value is the delay in milliseconds. Defaults to 0 (no delay). <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setTooltipDelay(String _tooltipDelay) {
+	    getStateHelper().put(PropertyKeys.tooltipDelay, _tooltipDelay);
+    }
+	
+
+	/**
+	 * The tooltip is hidden with a delay. This value is the delay in milliseconds. Defaults to 0 (no delay). <br />
+	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 */
+	public String getTooltipDelayHide() {
+		String value = (String)getStateHelper().eval(PropertyKeys.tooltipDelayHide);
+		return  value;
+	}
+	
+	/**
+	 * The tooltip is hidden with a delay. This value is the delay in milliseconds. Defaults to 0 (no delay). <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setTooltipDelayHide(String _tooltipDelayHide) {
+	    getStateHelper().put(PropertyKeys.tooltipDelayHide, _tooltipDelayHide);
+    }
+	
+
+	/**
+	 * The tooltip is shown with a delay. This value is the delay in milliseconds. Defaults to 0 (no delay). <br />
+	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 */
+	public String getTooltipDelayShow() {
+		String value = (String)getStateHelper().eval(PropertyKeys.tooltipDelayShow);
+		return  value;
+	}
+	
+	/**
+	 * The tooltip is shown with a delay. This value is the delay in milliseconds. Defaults to 0 (no delay). <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setTooltipDelayShow(String _tooltipDelayShow) {
+	    getStateHelper().put(PropertyKeys.tooltipDelayShow, _tooltipDelayShow);
+    }
+	
+
+	/**
+	 * Where is the tooltip to be displayed? Possible values: "top", "bottom", "right", "left", "auto", "auto top", "auto bottom", "auto right" and "auto left". Default to "bottom". <br />
+	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 */
+	public String getTooltipPosition() {
+		String value = (String)getStateHelper().eval(PropertyKeys.tooltipPosition);
+		return  value;
+	}
+	
+	/**
+	 * Where is the tooltip to be displayed? Possible values: "top", "bottom", "right", "left", "auto", "auto top", "auto bottom", "auto right" and "auto left". Default to "bottom". <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setTooltipPosition(String _tooltipPosition) {
+	    getStateHelper().put(PropertyKeys.tooltipPosition, _tooltipPosition);
+    }
+	
+}
+
---
+++ src/main/java/net/bootsfaces/render/IHasTooltip.java
@@ -0,0 +1,34 @@
+package net.bootsfaces.render;
+
+public interface IHasTooltip  {
+
+	/**
+	 * The text of the tooltip. <br />
+	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 */
+	public String getTooltip();
+
+	/**
+	 * The tooltip is shown and hidden with a delay. This value is the delay in milliseconds. Defaults to 0 (no delay). <br />
+	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 */
+	public String getTooltipDelay();
+
+	/**
+	 * The tooltip is hidden with a delay. This value is the delay in milliseconds. Defaults to 0 (no delay). <br />
+	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 */
+	public String getTooltipDelayHide();
+
+	/**
+	 * The tooltip is shown with a delay. This value is the delay in milliseconds. Defaults to 0 (no delay). <br />
+	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 */
+	public String getTooltipDelayShow();
+
+	/**
+	 * Where is the tooltip to be displayed? Possible values: "top", "bottom", "right", "left", "auto", "auto top", "auto bottom", "auto right" and "auto left". Default to "bottom". <br />
+	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 */
+	public String getTooltipPosition();
+}
