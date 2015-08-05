b37fc9658cd8113d6ea018d9238535f1921e96d5
stephan <webmaster1@beyondjava.de>
2015-08-06 00:05:28 +0200

    moved slider to new package

---
+++ src/main/java/net/bootsfaces/component/slider/Slider.java
@@ -0,0 +1,475 @@
+/**
+ *  Copyright 2014-15 by Riccardo Massera (TheCoder4.Eu) and Stephan Rauh (http://www.beyondjava.net).
+ *  
+ *  This file is part of BootsFaces.
+ *  
+ *  BootsFaces is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  BootsFaces is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with BootsFaces. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.bootsfaces.component.slider;
+
+import javax.faces.component.FacesComponent;
+import javax.faces.component.html.HtmlInputText;
+
+import net.bootsfaces.C;
+import net.bootsfaces.listeners.AddResourcesListener;
+import net.bootsfaces.render.A;
+import net.bootsfaces.render.Tooltip;
+
+/** This class holds the attributes of &lt;b:slider /&gt;. */
+@FacesComponent("net.bootsfaces.component.slider.Slider")
+public class Slider extends HtmlInputText implements net.bootsfaces.render.IHasTooltip {
+
+	public static final String COMPONENT_TYPE = "net.bootsfaces.component.slider.Slider";
+
+	public static final String COMPONENT_FAMILY = "net.bootsfaces.component";
+
+	public static final String DEFAULT_RENDERER = "net.bootsfaces.component.slider.Slider";
+
+	public Slider() {
+		AddResourcesListener.addResourceToHeadButAfterJQuery(C.BSF_LIBRARY, "jq/jquery.js");
+		Tooltip.addResourceFile();
+		setRendererType(DEFAULT_RENDERER);
+	}
+
+	public String getFamily() {
+		return COMPONENT_FAMILY;
+	}
+
+	/** Method added to prevent AngularFaces from setting the type */
+	public String getType() {
+		String mode = A.asString(getAttributes().get("mode"), "badge");
+		return mode.equals("edit") ? "text" : "hidden";
+	}
+
+	/**
+	 * Method added to prevent AngularFaces from setting the type
+	 * 
+	 * @param type
+	 *            this parameter is ignored
+	 */
+	public void setType(String type) {
+		// ignore the type - it's defined by the mode attribute
+
+	}
+
+	protected enum PropertyKeys {
+		binding, disabled, handleShape, handleSize, id, label, max, min, mode, orientation, step, style, styleClass, tooltip, tooltipDelay, tooltipDelayHide, tooltipDelayShow, tooltipPosition;
+
+		String toString;
+
+		PropertyKeys(String toString) {
+			this.toString = toString;
+		}
+
+		PropertyKeys() {
+		}
+
+		public String toString() {
+			return ((this.toString != null) ? this.toString : super.toString());
+		}
+	}
+
+	/**
+	 * An el expression referring to a server side UIComponent instance in a
+	 * backing bean. <br />
+	 * 
+	 * @return Returns the value of the attribute, or null, if it hasn't been
+	 *         set by the JSF file.
+	 */
+	public javax.faces.component.UIComponent getBinding() {
+		javax.faces.component.UIComponent value = (javax.faces.component.UIComponent) getStateHelper()
+				.eval(PropertyKeys.binding);
+		return value;
+	}
+
+	/**
+	 * An el expression referring to a server side UIComponent instance in a
+	 * backing bean. <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setBinding(javax.faces.component.UIComponent _binding) {
+		getStateHelper().put(PropertyKeys.binding, _binding);
+	}
+
+	/**
+	 * If true, you can't move the slider, nor can you edit the number. <br />
+	 * 
+	 * @return Returns the value of the attribute, or null, if it hasn't been
+	 *         set by the JSF file.
+	 */
+	public boolean isDisabled() {
+		Boolean value = (Boolean) getStateHelper().eval(PropertyKeys.disabled, false);
+		return (boolean) value;
+	}
+
+	/**
+	 * If true, you can't move the slider, nor can you edit the number. <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setDisabled(boolean _disabled) {
+		getStateHelper().put(PropertyKeys.disabled, _disabled);
+	}
+
+	/**
+	 * The default handle is squared. Specifing the "round" value for this
+	 * attribute will turn the handle shape to a circle. <br />
+	 * 
+	 * @return Returns the value of the attribute, or null, if it hasn't been
+	 *         set by the JSF file.
+	 */
+	public String getHandleShape() {
+		String value = (String) getStateHelper().eval(PropertyKeys.handleShape);
+		return value;
+	}
+
+	/**
+	 * The default handle is squared. Specifing the "round" value for this
+	 * attribute will turn the handle shape to a circle. <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setHandleShape(String _handleShape) {
+		getStateHelper().put(PropertyKeys.handleShape, _handleShape);
+	}
+
+	/**
+	 * In some situations (eg. mobile phones) the slider handle may be difficult
+	 * to operate because of its size. Specifing the "md" or "lg" value for this
+	 * attribute will change the handle size accordingly. <br />
+	 * 
+	 * @return Returns the value of the attribute, or null, if it hasn't been
+	 *         set by the JSF file.
+	 */
+	public String getHandleSize() {
+		String value = (String) getStateHelper().eval(PropertyKeys.handleSize);
+		return value;
+	}
+
+	/**
+	 * In some situations (eg. mobile phones) the slider handle may be difficult
+	 * to operate because of its size. Specifing the "md" or "lg" value for this
+	 * attribute will change the handle size accordingly. <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setHandleSize(String _handleSize) {
+		getStateHelper().put(PropertyKeys.handleSize, _handleSize);
+	}
+
+	/**
+	 * Unique identifier of the component in a namingContainer. <br />
+	 * 
+	 * @return Returns the value of the attribute, or null, if it hasn't been
+	 *         set by the JSF file.
+	 */
+	public String getId() {
+		String value = (String) getStateHelper().eval(PropertyKeys.id);
+		return value;
+	}
+
+	/**
+	 * Unique identifier of the component in a namingContainer. <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setId(String _id) {
+		getStateHelper().put(PropertyKeys.id, _id);
+	}
+
+	/**
+	 * Label for the widget field. <br />
+	 * 
+	 * @return Returns the value of the attribute, or null, if it hasn't been
+	 *         set by the JSF file.
+	 */
+	public String getLabel() {
+		String value = (String) getStateHelper().eval(PropertyKeys.label);
+		return value;
+	}
+
+	/**
+	 * Label for the widget field. <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setLabel(String _label) {
+		getStateHelper().put(PropertyKeys.label, _label);
+	}
+
+	/**
+	 * The maximum value of the slider. (default 100) <br />
+	 * 
+	 * @return Returns the value of the attribute, or null, if it hasn't been
+	 *         set by the JSF file.
+	 */
+	public int getMax() {
+		Integer value = (Integer) getStateHelper().eval(PropertyKeys.max, 100);
+		return (int) value;
+	}
+
+	/**
+	 * The maximum value of the slider. (default 100) <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setMax(int _max) {
+		getStateHelper().put(PropertyKeys.max, _max);
+	}
+
+	/**
+	 * The minimum value of the slider. (default 0) <br />
+	 * 
+	 * @return Returns the value of the attribute, or null, if it hasn't been
+	 *         set by the JSF file.
+	 */
+	public int getMin() {
+		Integer value = (Integer) getStateHelper().eval(PropertyKeys.min, 0);
+		return (int) value;
+	}
+
+	/**
+	 * The minimum value of the slider. (default 0) <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setMin(int _min) {
+		getStateHelper().put(PropertyKeys.min, _min);
+	}
+
+	/**
+	 * Mode of the Slider Widget.<br>
+	 * There are three modes available : badge, edit and basic.<br>
+	 * In <b>basic mode</b>, only the slider and the label(if present) will be
+	 * shown and the slider value will be hidden.<br>
+	 * In <b>badge mode</b>, the default, the slider value will be shown in a
+	 * badge.<br>
+	 * In <b>edit mode</b>, an editable input field showing the slider value
+	 * will be shown; in this mode you can change the value by sliding or
+	 * editing the value in the field. <br />
+	 * 
+	 * @return Returns the value of the attribute, or null, if it hasn't been
+	 *         set by the JSF file.
+	 */
+	public String getMode() {
+		String value = (String) getStateHelper().eval(PropertyKeys.mode, "badge");
+		return value;
+	}
+
+	/**
+	 * Mode of the Slider Widget.<br>
+	 * There are three modes available : badge, edit and basic.<br>
+	 * In <b>basic mode</b>, only the slider and the label(if present) will be
+	 * shown and the slider value will be hidden.<br>
+	 * In <b>badge mode</b>, the default, the slider value will be shown in a
+	 * badge.<br>
+	 * In <b>edit mode</b>, an editable input field showing the slider value
+	 * will be shown; in this mode you can change the value by sliding or
+	 * editing the value in the field. <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setMode(String _mode) {
+		getStateHelper().put(PropertyKeys.mode, _mode);
+	}
+
+	/**
+	 * Determines whether the slider handles move horizontally (min on left, max
+	 * on right) or vertically (min on bottom, max on top).<br>
+	 * Possible values: "horizontal"(default), "vertical", "vertical-bottom".
+	 * <br>
+	 * If <b>vertical</b> is specified, the Label is rendered on top, then the
+	 * value and the slider on the bottom.<br>
+	 * If <b>vertical-bottom</b> is specified, the slider is rendered on top,
+	 * then the value and the Label on the bottom. <br />
+	 * 
+	 * @return Returns the value of the attribute, or null, if it hasn't been
+	 *         set by the JSF file.
+	 */
+	public String getOrientation() {
+		String value = (String) getStateHelper().eval(PropertyKeys.orientation);
+		return value;
+	}
+
+	/**
+	 * Determines whether the slider handles move horizontally (min on left, max
+	 * on right) or vertically (min on bottom, max on top).<br>
+	 * Possible values: "horizontal"(default), "vertical", "vertical-bottom".
+	 * <br>
+	 * If <b>vertical</b> is specified, the Label is rendered on top, then the
+	 * value and the slider on the bottom.<br>
+	 * If <b>vertical-bottom</b> is specified, the slider is rendered on top,
+	 * then the value and the Label on the bottom. <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setOrientation(String _orientation) {
+		getStateHelper().put(PropertyKeys.orientation, _orientation);
+	}
+
+	/**
+	 * The step of the slider. <br />
+	 * 
+	 * @return Returns the value of the attribute, or null, if it hasn't been
+	 *         set by the JSF file.
+	 */
+	public int getStep() {
+		Integer value = (Integer) getStateHelper().eval(PropertyKeys.step, 0);
+		return (int) value;
+	}
+
+	/**
+	 * The step of the slider. <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setStep(int _step) {
+		getStateHelper().put(PropertyKeys.step, _step);
+	}
+
+	/**
+	 * Inline style of the input element. <br />
+	 * 
+	 * @return Returns the value of the attribute, or null, if it hasn't been
+	 *         set by the JSF file.
+	 */
+	public String getStyle() {
+		String value = (String) getStateHelper().eval(PropertyKeys.style);
+		return value;
+	}
+
+	/**
+	 * Inline style of the input element. <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setStyle(String _style) {
+		getStateHelper().put(PropertyKeys.style, _style);
+	}
+
+	/**
+	 * Style class of the input element. <br />
+	 * 
+	 * @return Returns the value of the attribute, or null, if it hasn't been
+	 *         set by the JSF file.
+	 */
+	public String getStyleClass() {
+		String value = (String) getStateHelper().eval(PropertyKeys.styleClass);
+		return value;
+	}
+
+	/**
+	 * Style class of the input element. <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setStyleClass(String _styleClass) {
+		getStateHelper().put(PropertyKeys.styleClass, _styleClass);
+	}
+
+	/**
+	 * The text of the tooltip. <br />
+	 * 
+	 * @return Returns the value of the attribute, or null, if it hasn't been
+	 *         set by the JSF file.
+	 */
+	public String getTooltip() {
+		String value = (String) getStateHelper().eval(PropertyKeys.tooltip);
+		return value;
+	}
+
+	/**
+	 * The text of the tooltip. <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setTooltip(String _tooltip) {
+		getStateHelper().put(PropertyKeys.tooltip, _tooltip);
+	}
+
+	/**
+	 * The tooltip is shown and hidden with a delay. This value is the delay in
+	 * milliseconds. Defaults to 0 (no delay). <br />
+	 * 
+	 * @return Returns the value of the attribute, or null, if it hasn't been
+	 *         set by the JSF file.
+	 */
+	public int getTooltipDelay() {
+		Integer value = (Integer) getStateHelper().eval(PropertyKeys.tooltipDelay, 0);
+		return (int) value;
+	}
+
+	/**
+	 * The tooltip is shown and hidden with a delay. This value is the delay in
+	 * milliseconds. Defaults to 0 (no delay). <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setTooltipDelay(int _tooltipDelay) {
+		getStateHelper().put(PropertyKeys.tooltipDelay, _tooltipDelay);
+	}
+
+	/**
+	 * The tooltip is hidden with a delay. This value is the delay in
+	 * milliseconds. Defaults to 0 (no delay). <br />
+	 * 
+	 * @return Returns the value of the attribute, or null, if it hasn't been
+	 *         set by the JSF file.
+	 */
+	public int getTooltipDelayHide() {
+		Integer value = (Integer) getStateHelper().eval(PropertyKeys.tooltipDelayHide, 0);
+		return (int) value;
+	}
+
+	/**
+	 * The tooltip is hidden with a delay. This value is the delay in
+	 * milliseconds. Defaults to 0 (no delay). <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setTooltipDelayHide(int _tooltipDelayHide) {
+		getStateHelper().put(PropertyKeys.tooltipDelayHide, _tooltipDelayHide);
+	}
+
+	/**
+	 * The tooltip is shown with a delay. This value is the delay in
+	 * milliseconds. Defaults to 0 (no delay). <br />
+	 * 
+	 * @return Returns the value of the attribute, or null, if it hasn't been
+	 *         set by the JSF file.
+	 */
+	public int getTooltipDelayShow() {
+		Integer value = (Integer) getStateHelper().eval(PropertyKeys.tooltipDelayShow, 0);
+		return (int) value;
+	}
+
+	/**
+	 * The tooltip is shown with a delay. This value is the delay in
+	 * milliseconds. Defaults to 0 (no delay). <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setTooltipDelayShow(int _tooltipDelayShow) {
+		getStateHelper().put(PropertyKeys.tooltipDelayShow, _tooltipDelayShow);
+	}
+
+	/**
+	 * Where is the tooltip to be displayed? Possible values: "top", "bottom",
+	 * "right", "left", "auto", "auto top", "auto bottom", "auto right" and
+	 * "auto left". Default to "bottom". <br />
+	 * 
+	 * @return Returns the value of the attribute, or null, if it hasn't been
+	 *         set by the JSF file.
+	 */
+	public String getTooltipPosition() {
+		String value = (String) getStateHelper().eval(PropertyKeys.tooltipPosition);
+		return value;
+	}
+
+	/**
+	 * Where is the tooltip to be displayed? Possible values: "top", "bottom",
+	 * "right", "left", "auto", "auto top", "auto bottom", "auto right" and
+	 * "auto left". Default to "bottom". <br />
+	 * Usually this method is called internally by the JSF engine.
+	 */
+	public void setTooltipPosition(String _tooltipPosition) {
+		getStateHelper().put(PropertyKeys.tooltipPosition, _tooltipPosition);
+	}
+
+}
---
+++ src/main/java/net/bootsfaces/component/slider/SliderRenderer.java
@@ -0,0 +1,296 @@
+/**
+ *  Copyright 2014-15 by Riccardo Massera (TheCoder4.Eu) and Stephan Rauh (http://www.beyondjava.net).
+ *  
+ *  This file is part of BootsFaces.
+ *  
+ *  BootsFaces is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  BootsFaces is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with BootsFaces. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.bootsfaces.component.slider;
+
+import javax.faces.component.*;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import javax.faces.render.FacesRenderer;
+
+import net.bootsfaces.C;
+import net.bootsfaces.render.A;
+import net.bootsfaces.render.CoreRenderer;
+import net.bootsfaces.render.JQ;
+import net.bootsfaces.render.R;
+import net.bootsfaces.render.Tooltip;
+
+
+/** This class generates the HTML code of &lt;b:slider /&gt;. */
+@FacesRenderer(componentFamily = "net.bootsfaces.component", rendererType = "net.bootsfaces.component.slider.Slider")
+public class SliderRenderer extends CoreRenderer {
+	/**
+	 * This methods receives and processes input made by the user. More specifically, it ckecks whether the
+	 * user has interacted with the current b:slider. The default implementation simply stores
+	 * the input value in the list of submitted values. If the validation checks are passed,
+	 * the values in the <code>submittedValues</code> list are store in the backend bean.
+	 * @param context the FacesContext.
+	 * @param component the current b:slider.
+	 */  
+	@Override
+	public void decode(FacesContext context, UIComponent component) {
+	    Slider slider = (Slider) component;
+	
+		if (slider.isDisabled() || slider.isReadonly()) {
+		    return;
+		}
+	
+	    decodeBehaviors(context, slider);
+	
+	    String clientId = slider.getClientId(context);
+	    String submittedValue = (String) context.getExternalContext().getRequestParameterMap().get(clientId);
+	
+	    if (submittedValue != null) {
+	    	slider.setSubmittedValue(submittedValue);
+	    	slider.setValid(true);
+	    }
+	}
+	
+	/**
+	 * This methods generates the HTML code of the current b:slider.
+	 * @param context the FacesContext.
+	 * @param component the current b:slider.
+	 * @throws IOException thrown if something goes wrong when writing the HTML code.
+	 */  
+	@Override
+	public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
+	    if (!component.isRendered()) {
+	        return;
+	    }
+		Slider slider = (Slider) component;
+		ResponseWriter rw = context.getResponseWriter();
+		encodeHTML(slider, context, rw);
+		
+		Tooltip.activateTooltips(context, slider);
+		
+	}
+	
+	private void encodeHTML(Slider slider, FacesContext context, ResponseWriter rw) throws IOException {
+		String clientId = slider.getClientId(context);
+
+		String mode = slider.getMode();
+		String label = slider.getLabel();
+
+		int min = slider.getMin();
+		int max = slider.getMax();
+		Object v = slider.getSubmittedValue();
+		if (v == null) {
+			v = slider.getValue();
+		}
+		if (v == null) {
+			v = slider.getValue();
+			slider.setValue(v);
+		}
+		if (v == null) {
+			v = max / 2;
+			slider.setValue(v);
+		}
+		int val = A.toInt(v);
+		if (val > max) {
+			val = max;
+		}
+		if (val < min) {
+			val = min;
+		}
+		String o;
+		if (slider.getOrientation() != null) {
+			o = slider.getOrientation();
+		} else {
+			o = C.H;
+		}
+		boolean vo = o.startsWith("vertical");
+		boolean bottom = o.endsWith("bottom");
+
+		rw.startElement("div", null);// form-group
+		Tooltip.generateTooltip(context, slider, rw);
+		rw.writeAttribute("class", "form-group", "class");
+		R.encodeRow(rw, null, null, (vo ? "slider-vertical" : "slider")); 
+		// -------------------------------------------------------------->
+		// <<-- Vertical -->>
+		if (vo) {
+			if (label != null && !bottom) {
+				R.encodeRow(rw, null, null, null);
+				encodeVLabel(slider, rw, label);
+				rw.endElement("div");/* Row */
+			}
+			R.encodeRow(rw, null, null, null);
+			if (bottom) {
+				encodeSliderDiv(rw, vo, clientId);
+				rw.endElement("div");/* Row */
+				R.encodeRow(rw, null, null, null);
+			}
+			encodeInput(slider, rw, mode, context, val, clientId, vo, min, max);
+			if (!bottom) {
+				rw.endElement("div"); /* Row */
+
+				R.encodeRow(rw, null, null, null);
+				encodeSliderDiv(rw, vo, clientId);
+			}
+			rw.endElement("div"); /* Row */
+			if (label != null && bottom) {
+				R.encodeRow(rw, null, null, null);
+				encodeVLabel(slider, rw, label);
+				rw.endElement("div"); /* Row */
+			}
+
+		} else {
+			// <<-- Horizontal -->>
+
+			if (label != null) {
+				R.encodeRow(rw, null, null, null);
+
+				R.encodeColumn(rw, null, 6, 6, 6, 6, 0, 0, 0, 0, null, null);
+				rw.startElement("label", slider);
+				rw.writeAttribute("for", clientId, null);
+				rw.write(label);
+				rw.endElement("label"); // Label
+
+				rw.endElement("div");// Column
+
+				rw.endElement("div");/* Row */
+			}
+			R.encodeRow(rw, null, null, null);
+
+			encodeInput(slider, rw, mode, context, val, clientId, vo, min, max);
+
+			encodeSliderDiv(rw, vo, clientId);
+			rw.endElement("div");/* Row */
+
+		} // if vo
+
+		// <<---------------------------------------
+		rw.endElement("div"); // rw.write("<!-- Slider Widget Row
+								// -->\n");//Slider Widget Row
+
+		rw.endElement("div"); // rw.write("<!-- form-group -->\n");//form-group
+
+		encodeJS(slider, rw, clientId);
+		Tooltip.activateTooltips(context, slider);
+	}
+
+	private void encodeVLabel(Slider slider, ResponseWriter rw, String label) throws IOException {
+		R.encodeColumn(rw, null, 12, 12, 12, 12, 0, 0, 0, 0, null, null);
+		rw.startElement("p", slider);
+		rw.write(label);
+		rw.endElement("p"); // Label
+		rw.endElement("div"); // Column
+	}
+
+	private void encodeInput(Slider slider, ResponseWriter rw, String mode, FacesContext context, int val, String clientId, boolean vo,
+			int min, int max) throws IOException {
+		int cols = (vo ? 12 : 1);
+		if (!mode.equals("basic")) {
+			/*
+			 * int span, int offset, int cxs, int csm, int clg, int oxs, int
+			 * osm, int olg
+			 */
+			R.encodeColumn(rw, null, cols, cols, cols, cols, 0, 0, 0, 0, null, null);
+			if (mode.equals("badge")) {
+				R.encodeBadge(context, slider, "_badge", Integer.toString(val));
+			}
+		}
+		removeMisleadingType(slider);
+		// Input
+		rw.startElement("input", slider);
+		rw.writeAttribute("id", clientId, null);
+		rw.writeAttribute("name", clientId, null);
+		rw.writeAttribute("type", (mode.equals("edit") ? "text" : "hidden"), null);
+		rw.writeAttribute("size", String.valueOf(max).length() - 1, null);
+		rw.writeAttribute("min", min, null);
+		rw.writeAttribute("max", max, null);
+		rw.writeAttribute("maxlength", String.valueOf(max).length(), null);
+
+		rw.writeAttribute("class", "form-control input-sm" + (vo ? " text-center" : ""), "class");
+
+		rw.writeAttribute("value", val, null);
+
+		// if (rdonly) { rw.writeAttribute(H.READONLY, H.READONLY, null); }
+		rw.endElement("input");
+
+		if (!mode.equals("basic")) {
+			rw.endElement("div");
+		} // Column
+	}
+
+	/**
+	 * remove wrong type information that may have been added by AngularFaces
+	 */
+	@SuppressWarnings("rawtypes")
+	private void removeMisleadingType(Slider slider) {
+		try {
+			Method method = getClass().getMethod("getPassThroughAttributes", (Class[])null);
+			if (null != method) {
+				Object map = method.invoke(this, (Object[])null);
+				if (null != map) {
+					Map attributes = (Map) map;
+					if (attributes.containsKey("type"))
+						attributes.remove("type");
+				}
+			}
+		} catch (ReflectiveOperationException ignoreMe) {
+			// we don't really have to care about this error
+		}
+
+	}
+
+	private void encodeSliderDiv(ResponseWriter rw, boolean vo, String clientId) throws IOException {
+		/*
+		 * int span, int offset, int cxs, int csm, int clg, int oxs, int osm,
+		 * int olg
+		 */
+		R.encodeColumn(rw, null, (vo ? 12 : 4), (vo ? 12 : 4), (vo ? 12 : 4), (vo ? 12 : 4), 0, 0, 0, 0, null, null);
+		// Slider <div>
+		rw.startElement("div", null);
+		rw.writeAttribute("id", clientId + "_slider", null);// concat
+																		// controproducente
+		rw.endElement("div");
+		rw.endElement("div"); // Column
+	}
+
+	private void encodeJS(Slider slider, ResponseWriter rw, String cId) throws IOException {
+		StringBuilder sb = new StringBuilder(100);
+		sb.append("value").append(":").append(slider.getValue()).append(",");
+		if (slider.getMax() > 0) {
+			sb.append("max").append(":").append(slider.getMax()).append(",");
+		}
+		sb.append("min").append(":").append(slider.getMin()).append(",");
+		if (slider.getOrientation() != null) {
+			String o = slider.getOrientation();
+			if (o.endsWith("bottom")) {
+				o = "vertical";
+			}
+			sb.append("orientation").append(":").append("'".concat(o).concat("'")).append(",");
+		}
+		if (slider.getStep() > 0) {
+			sb.append("step").append(":").append(slider.getStep()).append(",");
+		}
+		sb.append("range").append(":").append("\"min\"").append(",");
+
+		Map<String, Object> attributes = slider.getAttributes();
+		String hsize = A.asString(attributes.get("handle-size"));
+		String hshape = A.asString(attributes.get("handle-shape"));
+		boolean hround = ((hshape != null) && (hshape.equals("round")));
+
+		JQ.simpleSlider(rw, cId, sb.toString(), slider.getMode().equals("badge"), hsize, hround);
+	}
+}
