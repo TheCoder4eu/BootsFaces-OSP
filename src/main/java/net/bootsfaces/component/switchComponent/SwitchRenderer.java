b5507482e025f6cec7b5eda6c50f64c8f9a354e5
stephan <webmaster1@beyondjava.de>
2015-12-28 23:47:37 +0100

    #219 add more attributes to b:switch

--- src/main/java/net/bootsfaces/component/switchComponent/SwitchRenderer.java
+++ src/main/java/net/bootsfaces/component/switchComponent/SwitchRenderer.java
@@ -29,32 +29,52 @@ import javax.faces.render.FacesRenderer;
 import net.bootsfaces.component.selectBooleanCheckbox.SelectBooleanCheckbox;
 import net.bootsfaces.component.selectBooleanCheckbox.SelectBooleanCheckboxRenderer;
 
-
 /** This class generates the HTML code of &lt;b:switchWidget /&gt;. */
 @FacesRenderer(componentFamily = "net.bootsfaces.component", rendererType = "net.bootsfaces.component.switch.Switch")
 public class SwitchRenderer extends SelectBooleanCheckboxRenderer {
-	
+
 	@Override
 	public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
 		ResponseWriter rw = context.getResponseWriter();
 		String clientId = component.getClientId();
 		super.encodeBegin(context, component);
-		clientId=clientId.replace(":","\\\\:");
+		clientId = clientId.replace(":", "\\\\:");
 		rw.append("<script>");
 		rw.append("$('#input_" + clientId + "').bootstrapSwitch();");
 		rw.append("</script>");
 	}
-	
+
 	/**
-	 * The b:switch and the b:selectBooleanCheckbox share most of their code. This method allows to add extra attributes for the switch.
+	 * The b:switch and the b:selectBooleanCheckbox share most of their code.
+	 * This method allows to add extra attributes for the switch.
+	 * 
 	 * @param rw
 	 * @param selectBooleanCheckbox
 	 * @throws IOException
 	 */
-	protected void addAttributesForSwitch(ResponseWriter rw, SelectBooleanCheckbox selectBooleanCheckbox) throws IOException {
-		Switch switchComponent = (Switch)selectBooleanCheckbox;
+	protected void addAttributesForSwitch(ResponseWriter rw, SelectBooleanCheckbox selectBooleanCheckbox)
+			throws IOException {
+		Switch switchComponent = (Switch) selectBooleanCheckbox;
 		writeAttribute(rw, "data-off-text", switchComponent.getOffText());
 		writeAttribute(rw, "data-on-text", switchComponent.getOnText());
+		writeAttribute(rw, "data-on-color", switchComponent.getOnColor());
+		writeAttribute(rw, "data-off-color", switchComponent.getOffColor());
+		if (switchComponent.isIndeterminate())
+			writeAttribute(rw, "data-indeterminate", switchComponent.isIndeterminate());
+		if (switchComponent.isInverse())
+			writeAttribute(rw, "data-inverse", switchComponent.isInverse());
+		writeAttribute(rw, "data-size", switchComponent.getSwitchsize());
+		if (!switchComponent.isAnimate())
+			writeAttribute(rw, "data-animate", switchComponent.isAnimate());
+		if (switchComponent.isDisabled())
+			writeAttribute(rw, "data-disabled", switchComponent.isDisabled());
+		if (switchComponent.isReadonly())
+			writeAttribute(rw, "data-readonly", switchComponent.isReadonly());
+		writeAttribute(rw, "data-label-text", switchComponent.getLabelText());
+		if (switchComponent.getHandleWidth() > 0)
+			writeAttribute(rw, "data-handle-width", switchComponent.getHandleWidth());
+		if (switchComponent.getLabelWidth() > 0)
+			writeAttribute(rw, "data-label-width", switchComponent.getLabelWidth());
 	}
 
 }
