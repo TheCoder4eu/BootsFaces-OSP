29c6c21fe4dbe61742f548ad10e0e93654ae3c59
stephan <webmaster1@beyondjava.de>
2016-06-30 17:38:52 +0200

    #186 added the attributes icon-size, icon-flip, icon-rotate and icon-spin to every component that may have a Glyphicon or a Fontawesome icon

---
+++ src/main/java/net/bootsfaces/component/navCommandLink/NavCommandLinkBeanInfo.java
@@ -0,0 +1,18 @@
+package net.bootsfaces.component.navCommandLink;
+
+import net.bootsfaces.beans.BsfBeanInfo;
+
+/**
+ * BeanInfo class to provide mapping of snake-case attributes to camelCase ones
+ *
+ * @author durzod
+ */
+public class NavCommandLinkBeanInfo extends BsfBeanInfo {
+	/**
+	 * Get the reference decorated class
+	 */
+	@Override
+	public Class<?> getDecoratedClass() {
+		return NavCommandLink.class;
+	}
+}
