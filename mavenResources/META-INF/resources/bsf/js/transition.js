88109a57bd263d82bf56d61c01b719a8dd408c9c
stephan <webmaster1@beyondjava.de>
2016-09-13 22:36:38 +0200

    updated the Maven resource files

--- mavenResources/META-INF/resources/bsf/js/transition.js
+++ mavenResources/META-INF/resources/bsf/js/transition.js
@@ -1,9 +1,9 @@
 /*
  ========================================================================
- Bootstrap: transition.js v3.3.6
+ Bootstrap: transition.js v3.3.7
  http://getbootstrap.com/javascript/#transitions
  ========================================================================
- Copyright 2011-2015 Twitter, Inc.
+ Copyright 2011-2016 Twitter, Inc.
  Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  ======================================================================== */
 +function(a){function e(){var a=document.createElement("bootstrap"),b={WebkitTransition:"webkitTransitionEnd",MozTransition:"transitionend",OTransition:"oTransitionEnd otransitionend",transition:"transitionend"},c;for(c in b)if(void 0!==a.style[c])return{end:b[c]};return!1}a.fn.emulateTransitionEnd=function(d){var b=!1,c=this;a(this).one("bsTransitionEnd",function(){b=!0});setTimeout(function(){b||a(c).trigger(a.support.transition.end)},d);return this};a(function(){a.support.transition=e();a.support.transition&&
