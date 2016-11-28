737f45ce80b6504dead1896eb7e2810229288a65
stephan <webmaster1@beyondjava.de>
2016-11-28 23:13:37 +0100

    fixed the lower/upper case

---
+++ src/main/java/net/bootsfaces/component/datepicker/DatepickerBeanInfo.java
@@ -0,0 +1,36 @@
+/**
+ *  Copyright 2014-2016 Dario D'Urzo and Stephan Rauh
+ *  
+ *  This file is part of BootsFaces.
+ *  
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+ */
+package net.bootsfaces.component.datepicker;
+
+import net.bootsfaces.beans.BsfBeanInfo;
+
+/**
+ * BeanInfo class to provide mapping
+ * of snake-case attributes to camelCase ones
+ * 
+ * @author durzod
+ */
+public class DatepickerBeanInfo extends BsfBeanInfo {
+	/**
+	 * Get the reference decorated class
+	 */
+	@Override
+	public Class<?> getDecoratedClass() {
+		return Datepicker.class;
+	}
+}
