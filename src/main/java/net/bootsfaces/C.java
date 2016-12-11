874a0f5f023c53e3eb342a8838dca949a0ec6f22
Riccardo Massera (TheCoder4Eu) <thecoder4eu@gmail.com>
2016-12-11 17:12:10 +0100

    Automates the BootsFaces version number change in Java code when building with Gradle ( #555 )

--- src/main/java/net/bootsfaces/C.java
+++ src/main/java/net/bootsfaces/C.java
@@ -1,8 +1,8 @@
 /**
  *  Copyright 2013-2016 Riccardo Massera (TheCoder4.Eu)
- *  
+ *
  *  This file is part of BootsFaces.
- *  
+ *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
@@ -21,39 +21,41 @@ package net.bootsfaces;
 
 /**
  * String Constants valid for the whole Framework
- * 
+ *
  * @author thecoder4.eu
  */
 public final class C {
     /**
      * BootsFaces Library Constants
      */
-    public static final String BSFCOMPONENT= "net.bootsfaces.component";
-    public static final String BSFLAYOUT   = "net.bootsfaces.layout";
-    //public static final String BSFRENDER   = "net.bootsfaces.render";
-    
+     public static final String BSFVERSION="1.0.1";
+     public static final String BSFRELEASE_STATUS="SNAPSHOT"; //SNAPSHOT or RELEASE
+     public static final String BSFCOMPONENT= "net.bootsfaces.component";
+     public static final String BSFLAYOUT   = "net.bootsfaces.layout";
+     //public static final String BSFRENDER   = "net.bootsfaces.render";
+
     //Theme
     public static final String BSF_LIBRARY="bsf";
     public static final String P_USETHEME ="BootsFaces_USETHEME";
     public static final String P_THEME ="BootsFaces_THEME";
-    
+
     //Font Awesome
     //public static final String P_USEFONTAWESOME ="BootsFaces_USE_FA";
     public static final String FA_VERSION="4.7.0";
     public static final String FONTAWESOME_CDN_URL="//maxcdn.bootstrapcdn.com/font-awesome/"+FA_VERSION+"/css/font-awesome.min.css";
-    
+
     //Meta tags
-    // default yes. to disable set to 'no' or 'false'. if you want to change, set to the value you want to use 
-    public static final String P_VIEWPORT = "BootsFaces_USE_VIEWPORT"; 
-    
+    // default yes. to disable set to 'no' or 'false'. if you want to change, set to the value you want to use
+    public static final String P_VIEWPORT = "BootsFaces_USE_VIEWPORT";
+
     /**
      * Component Types
      */
     //Components
     public static final String W_NONAVCASE_LINK="This link is disabled because a navigation case could not be matched.";
     public static final String W_NONAVCASE_BUTTON="This link is disabled because a navigation case could not be matched.";
-    
-    
+
+
     // Suppress default constructor for noninstantiability
     private C() {
         throw new AssertionError();
--- src/main/java/net/bootsfaces/listeners/AddResourcesListener.java
+++ src/main/java/net/bootsfaces/listeners/AddResourcesListener.java
@@ -82,7 +82,8 @@ public class AddResourcesListener implements SystemEventListener {
 								CSS_RENDERER = "javax.faces.resource.Stylesheet";
 
 	static {
-		LOGGER.info("This application is running on BootsFaces 1.0.1-SNAPSHOT");
+		LOGGER.info("This application is running on BootsFaces"+C.BSFVERSION+"-"+C.BSFRELEASE_STATUS);
+		//LOGGER.info("This application is running on BootsFaces 1.0.1-SNAPSHOT");
 	}
 
 	/**
