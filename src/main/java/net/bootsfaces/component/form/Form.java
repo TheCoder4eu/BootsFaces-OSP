0d06f8c8abb06292aae65f78463633b1fc686c2f
stephan <webmaster1@beyondjava.de>
2017-05-02 20:40:01 +0200

    #747 fixed the regression of b:form in such a way that the changes "survive" code generation

--- src/main/java/net/bootsfaces/component/form/Form.java
+++ src/main/java/net/bootsfaces/component/form/Form.java
@@ -19,18 +19,17 @@
 package net.bootsfaces.component.form;
 
 import java.io.IOException;
+
 import javax.el.ValueExpression;
 import javax.faces.FacesException;
 import javax.faces.component.FacesComponent;
-import javax.faces.component.UIForm;
 import javax.faces.context.FacesContext;
-import net.bootsfaces.C;
 
 import net.bootsfaces.utils.BsfUtils;
 
 /** This class holds the attributes of &lt;b:form /&gt;. */
 @FacesComponent(Form.COMPONENT_TYPE)
-public class Form extends UIForm {
+public class Form extends FormCore {
 
 	public static final String COMPONENT_TYPE = "net.bootsfaces.component.form.Form";
 
@@ -54,84 +53,38 @@ public class Form extends UIForm {
 		super.setValueExpression(name, binding);
 	}
 
-	protected enum PropertyKeys {
-		horizontal, inline, style, styleClass;
-		String toString;
-
-		PropertyKeys(String toString) {
-			this.toString = toString;
-		}
-
-		PropertyKeys() {
-		}
-
-		public String toString() {
-			return ((this.toString != null) ? this.toString : super.toString());
-		}
-	}
-
 	/**
-	 * Use this flag to create a horizontal form (labels are on the same line as their input fields) <P>
-	 * @return Returns the value of the attribute, or false, if it hasn't been set by the JSF file.
-	 */
-	public boolean isHorizontal() {
-		return (boolean) (Boolean) getStateHelper().eval(PropertyKeys.horizontal, false);
-	}
-
-	/**
-	 * Use this flag to create a horizontal form (labels are on the same line as their input fields) <P>
-	 * Usually this method is called internally by the JSF engine.
-	 */
-	public void setHorizontal(boolean _horizontal) {
-		getStateHelper().put(PropertyKeys.horizontal, _horizontal);
-	}
-
-	/**
-	 * Use this flag to create a inline form (labels are on the same line as their input fields) <P>
-	 * @return Returns the value of the attribute, or false, if it hasn't been set by the JSF file.
-	 */
-	public boolean isInline() {
-		return (boolean) (Boolean) getStateHelper().eval(PropertyKeys.inline, false);
-	}
-
-	/**
-	 * Use this flag to create a inline form (labels are on the same line as their input fields) <P>
-	 * Usually this method is called internally by the JSF engine.
-	 */
-	public void setInline(boolean _inline) {
-		getStateHelper().put(PropertyKeys.inline, _inline);
-	}
-
-	/**
-	 * Inline style of the input element. <P>
-	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
-	 */
-	public String getStyle() {
-		return (String) getStateHelper().eval(PropertyKeys.style);
-	}
-
-	/**
-	 * Inline style of the input element. <P>
-	 * Usually this method is called internally by the JSF engine.
-	 */
-	public void setStyle(String _style) {
-		getStateHelper().put(PropertyKeys.style, _style);
-	}
-
-	/**
-	 * Style class of this element. <P>
-	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
+	 * Inline style of the input element.
+	 * <P>
+	 * 
+	 * @return Returns the value of the attribute, or null, if it hasn't been
+	 *         set by the JSF file.
 	 */
 	public String getStyleClass() {
-		return (String) getStateHelper().eval(PropertyKeys.styleClass);
+		String clazz = super.getStyleClass();
+		if (isHorizontal()) {
+			if (clazz == null) {
+				clazz = "form-horizontal";
+			} else {
+				clazz += " form-horizontal";
+			}
+		}
+		if (isInline()) {
+			if (clazz == null) {
+				clazz = "form-inline";
+			} else {
+				clazz += " form-inline";
+			}
+		}
+		return clazz;
 	}
 
-	/**
-	 * Style class of this element. <P>
-	 * Usually this method is called internally by the JSF engine.
-	 */
-	public void setStyleClass(String _styleClass) {
-		getStateHelper().put(PropertyKeys.styleClass, _styleClass);
+	public void encodeBegin(FacesContext context) throws IOException {
+		if (isHorizontal() && isInline()) {
+			throw new FacesException(
+					"A b:form can't be form both inline and horizontal. Please set only one of them for form \""
+							+ getClientId() + "\".");
+		}
+		super.encodeBegin(context);
 	}
-
 }
