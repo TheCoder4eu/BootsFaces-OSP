cd333a82ce2775c3b509436ecb81311ded122905
stephan <webmaster1@beyondjava.de>
2017-08-12 11:28:13 +0200

    Corrected the license (used to be GNU; now it's Apache V2)

--- src/main/java/net/bootsfaces/component/slider2/Slider2.java
+++ src/main/java/net/bootsfaces/component/slider2/Slider2.java
@@ -3,18 +3,17 @@
  *
  *  This file is part of BootsFaces.
  *
- *  BootsFaces is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  BootsFaces is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with BootsFaces. If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package net.bootsfaces.component.slider2;
@@ -40,34 +39,36 @@ public class Slider2 extends Slider2Core {
 	public Slider2() {
 		Tooltip.addResourceFiles();
 		AddResourcesListener.addThemedCSSResource("core.css");
-		//AddResourcesListener.addThemedCSSResource("bsf.css");
+		// AddResourcesListener.addThemedCSSResource("bsf.css");
 		AddResourcesListener.addExtCSSResource("bootstrap-slider.min.css");
 		AddResourcesListener.addResourceToHeadButAfterJQuery(C.BSF_LIBRARY, "js/bootstrap-slider.min.js");
 		setRendererType(DEFAULT_RENDERER);
 	}
 
-
-	   /**
-     * <p>Return <code>true</code> if the new value is different from
-     * the previous value.  First compare the two values by passing
-     * <em>value</em> to the <code>equals</code> method on argument
-     * <em>previous</em>.  If that method returns <code>true</code>,
-     * return <code>true</code>.  If that method returns
-     * <code>false</code>, and both arguments implement
-     * <code>java.lang.Comparable</code>, compare the two values by
-     * passing <em>value</em> to the <code>compareTo</code> method on
-     * argument <em>previous</em>.  Return <code>true</code> if this
-     * method returns <code>0</code>, <code>false</code> otherwise.</p>
-     *
-     * @param previous old value of this component (if any)
-     * @param value    new value of this component (if any)
-     */
-    protected boolean compareValues(Object previous, Object value) {
-    	if (previous instanceof String && value instanceof Double) {
-    		previous = Double.valueOf((String)previous);
-    	}
-    	return super.compareValues(previous, value);
-    }
+	/**
+	 * <p>
+	 * Return <code>true</code> if the new value is different from the previous
+	 * value. First compare the two values by passing <em>value</em> to the
+	 * <code>equals</code> method on argument <em>previous</em>. If that method
+	 * returns <code>true</code>, return <code>true</code>. If that method returns
+	 * <code>false</code>, and both arguments implement
+	 * <code>java.lang.Comparable</code>, compare the two values by passing
+	 * <em>value</em> to the <code>compareTo</code> method on argument
+	 * <em>previous</em>. Return <code>true</code> if this method returns
+	 * <code>0</code>, <code>false</code> otherwise.
+	 * </p>
+	 *
+	 * @param previous
+	 *            old value of this component (if any)
+	 * @param value
+	 *            new value of this component (if any)
+	 */
+	protected boolean compareValues(Object previous, Object value) {
+		if (previous instanceof String && value instanceof Double) {
+			previous = Double.valueOf((String) previous);
+		}
+		return super.compareValues(previous, value);
+	}
 
 	public String getFamily() {
 		return COMPONENT_FAMILY;
@@ -80,37 +81,26 @@ public class Slider2 extends Slider2Core {
 		name = BsfUtils.snakeCaseToCamelCase(name);
 		super.setValueExpression(name, binding);
 	}
-	/* !
-	@Override
-	public String getValue() {
-		Object val = getStateHelper().eval(PropertyKeys.value, "5");
-		if(val instanceof Number)
-			return val.toString();
-		return (String) val;
-	}
-
-	@Override
-	public double getMax() {
-		Object max = getStateHelper().eval(PropertyKeys.max, 100);
-		if(max instanceof Integer) return ((Integer) max).doubleValue();
-		else if(max instanceof String) return Double.parseDouble((String)max);
-		return (Double) max;
-	}
-
-	@Override
-	public double getMin() {
-		Object min = getStateHelper().eval(PropertyKeys.min, 0);
-		if(min instanceof Integer) return ((Integer) min).doubleValue();
-		else if(min instanceof String) return Double.parseDouble((String)min);
-		return (Double) min;
-	}
-
-	@Override
-	public double getStep() {
-		Object step = getStateHelper().eval(PropertyKeys.step, 1);
-		if(step instanceof Integer) return ((Integer) step).doubleValue();
-		else if(step instanceof String) return Double.parseDouble((String)step);
-		return (Double) step;
-	}
-	*/
+	/*
+	 * !
+	 * 
+	 * @Override public String getValue() { Object val =
+	 * getStateHelper().eval(PropertyKeys.value, "5"); if(val instanceof Number)
+	 * return val.toString(); return (String) val; }
+	 * 
+	 * @Override public double getMax() { Object max =
+	 * getStateHelper().eval(PropertyKeys.max, 100); if(max instanceof Integer)
+	 * return ((Integer) max).doubleValue(); else if(max instanceof String) return
+	 * Double.parseDouble((String)max); return (Double) max; }
+	 * 
+	 * @Override public double getMin() { Object min =
+	 * getStateHelper().eval(PropertyKeys.min, 0); if(min instanceof Integer) return
+	 * ((Integer) min).doubleValue(); else if(min instanceof String) return
+	 * Double.parseDouble((String)min); return (Double) min; }
+	 * 
+	 * @Override public double getStep() { Object step =
+	 * getStateHelper().eval(PropertyKeys.step, 1); if(step instanceof Integer)
+	 * return ((Integer) step).doubleValue(); else if(step instanceof String) return
+	 * Double.parseDouble((String)step); return (Double) step; }
+	 */
 }
