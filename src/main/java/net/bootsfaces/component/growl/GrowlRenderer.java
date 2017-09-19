3dc3a1bce5fce04e59116c7dc36e13c5538e879e
stephan <webmaster1@beyondjava.de>
2017-09-19 22:09:49 +0200

    #864 added the attributes style and style-class

--- src/main/java/net/bootsfaces/component/growl/GrowlRenderer.java
+++ src/main/java/net/bootsfaces/component/growl/GrowlRenderer.java
@@ -36,6 +36,21 @@ import net.bootsfaces.utils.BsfUtils;
  */
 @FacesRenderer(componentFamily=C.BSFCOMPONENT, rendererType="net.bootsfaces.component.GrowlRenderer")
 public class GrowlRenderer extends CoreMessageRenderer {
+	
+	static final String TEMPLATE = 
+			"<div data-notify='container' class='col-xs-11 col-sm-3 alert alert-{0} {9}' role='alert'>\n" + 
+		    "<div {8}>" +
+			"	<button type='button' aria-hidden='true' class='close' data-notify='dismiss'>×</button>\n" + 
+			"	<span data-notify='icon'></span>\n" + 
+			"	<span data-notify='title'>{1}</span>\n" + 
+			"	<span data-notify='message'>{2}</span>\n" + 
+			"	<div class='progress' data-notify='progressbar'>\n" + 
+			"		<div class='progress-bar progress-bar-{0}' role='progressbar' aria-valuenow='0' aria-valuemin='0' aria-valuemax='100' style='width: 0%;'></div>\n" + 
+			"	</div>\n" + 
+			"	<a href='{3}' target='{4}' data-notify='url'></a>\n" + 
+			"</div>"+
+			"</div>";
+	
     @Override
     public void encodeEnd(FacesContext facesContext, UIComponent component) throws IOException {
         if (!component.isRendered()){
@@ -102,28 +117,46 @@ public class GrowlRenderer extends CoreMessageRenderer {
         
         // get icon for message
         String icon = uiGrowl.getIcon() != null ? "fa fa-" + uiGrowl.getIcon() : getSeverityIcon(msg);
+        
+        String template = null;
+        if (uiGrowl.getStyle()!= null) {
+        		template = TEMPLATE.replace("{8}", "style='" + uiGrowl.getStyle() + "'");
+        }
+        if (uiGrowl.getStyleClass() != null) {
+	        	if (null == template) {
+	        		template = TEMPLATE;
+	        }
+	        	template = template.replace("{9}", uiGrowl.getStyleClass());
+        }
+        if (null == template) {
+         	template="";
+        }	else {
+        		template = ", template: \"" + template.replace("{8}", "").replace("{9}", "").replace("\n", "") + "\"";
+        		System.out.println(template);
+        }
 
         writer.writeText("" +
-				" 	$.notify({" + 
-				" 		title: '" + (uiGrowl.isShowSummary() ? summary : "") + "', " +
-				" 		message: '" + (uiGrowl.isShowDetail() ? detail : "") + "', " +
-				" 		icon: '" +  icon + "'" +
-				" 	}, {" + 
-				" 		position: null, " + 
-				" 		type: '" + messageType + "', " +
-				"		allow_dismiss: " + uiGrowl.isAllowDismiss() + ", " +
-				"		newest_on_top: " + uiGrowl.isNewestOnTop() + ", " + 
-				"       delay: " + uiGrowl.getDelay() + ", " + 
-				"       timer: " + uiGrowl.getTimer() + ", " + 
-				" 		placement: { " + 
-				"			from: '" + uiGrowl.getPlacementFrom() + "'," + 
-				"			align: '" + uiGrowl.getPlacementAlign() + "'" + 
-				" 		}, " + 
-				"		animate: { " + 
-				"			enter: '" + uiGrowl.getAnimationEnter() + "', " +
-				"			exit: '" + uiGrowl.getAnimationExit() + "' " +
-				"		} " + 
-				"   }); " +
+				"$.notify({" + 
+				"	title: '" + (uiGrowl.isShowSummary() ? summary : "") + "', " +
+				"	message: '" + (uiGrowl.isShowDetail() ? detail : "") + "', " +
+				"	icon: '" +  icon + "'" +
+				"}, {" + 
+				"	position: null, " + 
+				"	type: '" + messageType + "', " +
+				"	allow_dismiss: " + uiGrowl.isAllowDismiss() + ", " +
+				"	newest_on_top: " + uiGrowl.isNewestOnTop() + ", " + 
+				"     delay: " + uiGrowl.getDelay() + ", " + 
+				"     timer: " + uiGrowl.getTimer() + ", " + 
+				"	placement: { " + 
+				"		from: '" + uiGrowl.getPlacementFrom() + "'," + 
+				"		align: '" + uiGrowl.getPlacementAlign() + "'" + 
+				"	}, " + 
+				"	animate: { " + 
+				"		enter: '" + uiGrowl.getAnimationEnter() + "', " +
+				"		exit: '" + uiGrowl.getAnimationExit() + "' " +
+				"	} " + 
+				"     " + template +
+				" }); " +
 				"", null);
     }
 
@@ -160,4 +193,6 @@ public class GrowlRenderer extends CoreMessageRenderer {
 			return "info";
 		return "success";
 	}
+	
+	
 }
