2cc4c4a6471a7f43d508448c52894debe457f55e
jens <webmaster@mtv-gross-buchholz.de>
2019-10-25 15:28:23 +0200

    New CDN Datatable URLs
    This fixes the strange design of the filter field. ColGroup-Plugin added to CDN url. Also removed the duplicate datatable.min.js reference
    
    Fixes TheCoder4eu/BootsFaces-OSP#1100

--- mavenResources/META-INF/resources/bsf/js/alert.js
+++ mavenResources/META-INF/resources/bsf/js/alert.js
@@ -1,12 +1,12 @@
 /*
  ========================================================================
- Bootstrap: alert.js v3.3.7
- http://getbootstrap.com/javascript/#alerts
+ Bootstrap: alert.js v3.4.1
+ https://getbootstrap.com/docs/3.4/javascript/#alerts
  ========================================================================
- Copyright 2011-2016 Twitter, Inc.
+ Copyright 2011-2019 Twitter, Inc.
  Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  ======================================================================== */
 var process=process||{env:{NODE_ENV:"development"}};
-+function($){var dismiss='[data-dismiss="alert"]';var Alert=function(el){$(el).on("click",dismiss,this.close)};Alert.VERSION="3.3.7";Alert.TRANSITION_DURATION=150;Alert.prototype.close=function(e){var $this=$(this);var selector=$this.attr("data-target");if(!selector){selector=$this.attr("href");selector=selector&&selector.replace(/.*(?=#[^\s]*$)/,"")}var $parent=$(selector==="#"?[]:selector);if(e)e.preventDefault();if(!$parent.length)$parent=$this.closest(".alert");$parent.trigger(e=$.Event("close.bs.alert"));
-if(e.isDefaultPrevented())return;$parent.removeClass("in");function removeElement(){$parent.detach().trigger("closed.bs.alert").remove()}$.support.transition&&$parent.hasClass("fade")?$parent.one("bsTransitionEnd",removeElement).emulateTransitionEnd(Alert.TRANSITION_DURATION):removeElement()};function Plugin(option){return this.each(function(){var $this=$(this);var data=$this.data("bs.alert");if(!data)$this.data("bs.alert",data=new Alert(this));if(typeof option=="string")data[option].call($this)})}
-var old=$.fn.alert;$.fn.alert=Plugin;$.fn.alert.Constructor=Alert;$.fn.alert.noConflict=function(){$.fn.alert=old;return this};$(document).on("click.bs.alert.data-api",dismiss,Alert.prototype.close)}(jQuery);
++function($){var dismiss='[data-dismiss="alert"]';var Alert=function(el){$(el).on("click",dismiss,this.close)};Alert.VERSION="3.4.1";Alert.TRANSITION_DURATION=150;Alert.prototype.close=function(e){var $this=$(this);var selector=$this.attr("data-target");if(!selector){selector=$this.attr("href");selector=selector&&selector.replace(/.*(?=#[^\s]*$)/,"")}selector=selector==="#"?[]:selector;var $parent=$(document).find(selector);if(e)e.preventDefault();if(!$parent.length)$parent=$this.closest(".alert");
+$parent.trigger(e=$.Event("close.bs.alert"));if(e.isDefaultPrevented())return;$parent.removeClass("in");function removeElement(){$parent.detach().trigger("closed.bs.alert").remove()}$.support.transition&&$parent.hasClass("fade")?$parent.one("bsTransitionEnd",removeElement).emulateTransitionEnd(Alert.TRANSITION_DURATION):removeElement()};function Plugin(option){return this.each(function(){var $this=$(this);var data=$this.data("bs.alert");if(!data)$this.data("bs.alert",data=new Alert(this));if(typeof option==
+"string")data[option].call($this)})}var old=$.fn.alert;$.fn.alert=Plugin;$.fn.alert.Constructor=Alert;$.fn.alert.noConflict=function(){$.fn.alert=old;return this};$(document).on("click.bs.alert.data-api",dismiss,Alert.prototype.close)}(jQuery);
