eca3f43718f75275a9a138d8211e00fe055eebad
jens <info@sebsoftware.de>
2020-04-30 23:10:45 +0200

    Fix for icons assigned by valueExpression
    if a valueExpression was used for properties that toggles the inclusion of scripts or css if a constant value is passed these inclusion were not added . By adding some additional code to setValueExpression these forces adding the required libraries, even if the final value may be empty or null, which isn't known during set.
    
    Fixes #1098
    Fixes #1052

--- src/main/java/net/bootsfaces/component/dropMenu/DropMenu.java
+++ src/main/java/net/bootsfaces/component/dropMenu/DropMenu.java
@@ -61,6 +61,11 @@ public class DropMenu extends DropMenuCore implements IHasTooltip, IResponsive, 
 	public void setValueExpression(String name, ValueExpression binding) {
 		name = BsfUtils.snakeCaseToCamelCase(name);
 		super.setValueExpression(name, binding);
+		
+		if ("iconAwesome".equals(name))
+			AddResourcesListener.setNeedsFontsAwesome(this);
+		else if ("iconBrand".equals(name) || "iconLight".equals(name) || "iconRegular".equals(name) || "iconSolid".equals(name))
+			AddResourcesListener.setFontAwesomeVersion(5, this);
 	}
 
 	public void processEvent(ComponentSystemEvent event) throws AbortProcessingException {
