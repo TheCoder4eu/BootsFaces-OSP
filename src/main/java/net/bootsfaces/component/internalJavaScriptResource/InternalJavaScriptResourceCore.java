befda179a3aebf504dfb66108ff984984c79a075
geopossachs <10324450+geopossachs@users.noreply.github.com>
2021-10-18 21:44:17 +0200

    clean up the code

--- src/main/java/net/bootsfaces/component/internalJavaScriptResource/InternalJavaScriptResourceCore.java
+++ src/main/java/net/bootsfaces/component/internalJavaScriptResource/InternalJavaScriptResourceCore.java
@@ -3,19 +3,18 @@
  *
  *  This file is part of BootsFaces.
  *
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package net.bootsfaces.component.internalJavaScriptResource;
 
 import javax.faces.component.UIOutput;
@@ -36,13 +35,15 @@ public abstract class InternalJavaScriptResourceCore extends UIOutput {
 		PropertyKeys() {
 		}
 
+		@Override
 		public String toString() {
 			return ((this.toString != null) ? this.toString : super.toString());
 		}
 	}
 
 	/**
-	 * URL of the JavaScript file to include. <P>
+	 * URL of the JavaScript file to include.
+	 * <P>
 	 * @return Returns the value of the attribute, or null, if it hasn't been set by the JSF file.
 	 */
 	public String getUrl() {
@@ -50,8 +51,11 @@ public abstract class InternalJavaScriptResourceCore extends UIOutput {
 	}
 
 	/**
-	 * URL of the JavaScript file to include. <P>
+	 * URL of the JavaScript file to include.
+	 * <P>
 	 * Usually this method is called internally by the JSF engine.
+	 *
+	 * @param _url
 	 */
 	public void setUrl(String _url) {
 		getStateHelper().put(PropertyKeys.url, _url);
